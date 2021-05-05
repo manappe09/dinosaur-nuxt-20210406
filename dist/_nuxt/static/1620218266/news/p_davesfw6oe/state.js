4d7379d6bdfce6069371f10b22551e39a220a246
manappe09 <manape130@gmail.com>
2021-05-06 04:28:44 +0900

    再ビルド

--- dist/_nuxt/static/1620218266/news/p_davesfw6oe/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620218266",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002Fnews\u002Fp_davesfw6oe",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1620242819/museum/ts_pby941/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620242819",layout:"default",error:a,state:{counter:0,todos:{list:[]}},serverRendered:true,routePath:"\u002Fmuseum\u002Fts_pby941",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));

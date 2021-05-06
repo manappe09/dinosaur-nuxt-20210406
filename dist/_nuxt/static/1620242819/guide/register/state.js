8ce0406469534881ceb8a603ae61b67c18c99333
manappe09 <manape130@gmail.com>
2021-05-07 04:10:15 +0900

    generateし直し

--- dist/_nuxt/static/1620242819/guide/register/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620242819",layout:"default",error:a,state:{counter:0,todos:{list:[]}},serverRendered:true,routePath:"\u002Fguide\u002Fregister",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));

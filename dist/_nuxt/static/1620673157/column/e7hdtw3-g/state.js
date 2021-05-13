e23046c3864dada40cf3edd47dec7e53d391f3b3
manappe09 <manape130@gmail.com>
2021-05-14 05:51:18 +0900

    generateし直し

--- dist/_nuxt/static/1620673157/column/e7hdtw3-g/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620673157",layout:"default",error:a,state:{filter:{selectedValue:{categories:[],ages:[]},filteringBoxState:false},todos:{list:[]}},serverRendered:true,routePath:"\u002Fcolumn\u002Fe7hdtw3-g",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
